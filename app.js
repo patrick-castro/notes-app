3ba4d725c77c8df2f89e190ffb480276893beac4
patrick-castro <castropatrickantonio@gmail.com>
2020-04-10 23:27:20 +0800

    Added an add note feature

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const yargs = require('yargs');
 const note = require('./notes.js');
 
-console.log(yargs);
-console.log(yargs.argv);
-// console.log(note.testFunc());
+//Added a command to add a new note
+//Sample input: node app.js add --title="Grocery List" --body="Cereal and milk"
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Insert title of the new note',
+            demandOption: true,
+            type: 'string',
+        },
+        body: {
+            describe: 'Insert content of the new note',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        note.addNote(argv.title, argv.body);
+    },
+});
 
 yargs.parse();
--- notes.js
+++ notes.js
@@ -1,10 +1,40 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const testFunc = function () {
-    console.log('Notes file is working successfully!');
-}
+// Prompt messages styling.
+const successMsg = chalk.green.inverse;
+const errorMsg = chalk.red.inverse;
+
+// Main function for adding note.
+const addNote = function (title, body) {
+    const notes = loadNotes();
+    const duplicateNote = notes.find((note) => note.title === title);
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        });
+
+        saveNote(notes);
+        console.log(successMsg('Successfully added a note!'));
+    } else {
+        console.log(errorMsg('The note already exists!'));
+    }
+};
+
+// Reads the stated json file. If it doesn't exist, the function returns an empty array.
+const loadNotes = function () {
+    try {
+        const bufferedData = fs.readFileSync('notes.json');
+        return JSON.parse(bufferedData);
+    } catch (e) {
+        return [];
+    }
+};
+
+// Writes/Overwrite to the json file. If it doesn't exist, fs will create one.
+const saveNote = (notes) => fs.writeFileSync('notes.json', JSON.stringify(notes));
 
 module.exports = {
-    testFunc: testFunc
+    addNote: addNote
 }
